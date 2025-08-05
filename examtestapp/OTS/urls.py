a60e3fef2d86d788dacd6302ca282ce9a9cb0cc2
sagarsaini010 <sagarsaini954818@gmail.com>
2025-08-05 16:20:46 +0530

    Today complete OTS project

--- examtestapp/OTS/urls.py
+++ examtestapp/OTS/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('login', loginView, name='login'),
     path('home', candidateHome, name='home'),
     path('test-paper', testPaper, name='testPaper'),
-    path('calculate-result', calculateTestResult),
+    path('calculate-result', calculateTestResult, name='calculateTest'),
     path('test-history', testResultHistory, name='testHistory'),
-    path('result', showTestResult),
+    path('result', showTestResult, name = 'result'),
     path('logout', logoutView, name='logout'),
 ]
